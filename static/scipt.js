ed97f67f8ce77232e9aef45feccfb2230be2c7f6
SemTheLegend <semmathe@gmail.com>
2023-08-14 10:33:07 +0200

    added a comment in line 3 script file

--- static/scipt.js
+++ static/scipt.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const button = document.querySelectorAll("button");
+const button = document.querySelectorAll("button"); // Returns an Array
 
 for (let i = 0; i < button.length; i++) {
   button[i].addEventListener("click", () => {
